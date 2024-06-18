c2d37d57e874a8fb20d1692092c6852191f23f72
Designer-HTi <designer.hti@icloud.com>
2024-06-19 04:51:04 +0800

    登录样式修改

--- src/renderer/views/Login/index.vue
+++ src/renderer/views/Login/index.vue
@@ -1,27 +1,24 @@
 <template>
   <div class="h-100vh w-full flex justify-center items-center">
     <div
-      class="w-full h-full bg-Layout-Card border-rd-4 border-2 border-Layout-White flex-col justify-center shadow-2xl"
+      class="w-full h-full bg-Layout-Card border-rd-4 border-2 overflow-hidden border-Layout-White flex-col justify-center shadow-2xl"
     >
-      <div v-if="isElectron" class="self-end flex p-3">
+      <div v-if="isElectron" class="w-full flex p-3 justify-end bg-Layout-Sidebar drag">
+        <!-- <div class="flex p-3 justify-end"> -->
         <iconBtn
           icon-name="subtract-line"
           type="WindowsControls"
           size="lg"
           @click="setWindowSize('min')"
         ></iconBtn>
-        <iconBtn
-          icon-name="aspect-ratio-line"
-          type="WindowsControls"
-          size="lg"
-          @click="setWindowSize('max')"
-        ></iconBtn>
+        <iconBtn icon-name="aspect-ratio-line" type="WindowsControls" size="lg"></iconBtn>
         <iconBtn
           icon-name="close-line"
           type="WindowsControls"
           size="lg"
           @click="setWindowSize('close')"
         ></iconBtn>
+        <!-- </div> -->
       </div>
       <div class="w-full flex items-center flex-1 gap-x-80px">
         <div class="w-63 p-4 ml-20 grid h-400px">
@@ -98,5 +95,8 @@ const setWindowSize = (type: string) => {
 .login-img {
   background: url('../../assets/img/LOGO.png') no-repeat;
   background-size: 100% 100%;
+}
+.drag {
+  -webkit-app-region: drag;
 }
 </style>
